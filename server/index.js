907c680a63efc195ed24e266a3b4fb39ac75bc22
pschaefferkoetter <prschaefferkoetter@gmail.com>
2022-04-27 16:18:41 -0600

    add serverio endpoint

--- server/index.js
+++ server/index.js
@@ -13,6 +13,13 @@ app.use(express.json());
 // use the routes to handle endpoints
 app.use(products);
 
+// create a route for serverio
+const loaderIoUrl = 'http://54.193.77.39:3001/loaderio-3a29069b26f647aa5accd4f94aa0935e.html';
+app.get(loaderIoUrl, (req, res) => {
+  const file = '../loaderio-3a29069b26f647aa5accd4f94aa0935e.txt';
+  res.sendFile(file);
+});
+
 const PORT = 3001;
 app.listen(PORT, () => {
   console.log(`server connected to port: ${PORT}`);
