33208e08113e1d36fcd6ec36ea9e368cd14b693e
pschaefferkoetter <prschaefferkoetter@gmail.com>
2022-04-28 09:12:24 -0600

    update dotenv path

--- server/db/index.js
+++ server/db/index.js
@@ -1,16 +1,19 @@
 const { Pool } = require('pg');
-require('dotenv').config();
+require('dotenv').config({ path: '../.env' });
 
-const {DBPORT, DBNAME, USER, HOST, PASSWORD } = process.env;
+const { DBPORT, USER, HOST, DBNAME, PASSWORD } = process.env;
+console.log(process.env.DBNAME);
+
 // create an instance
-// need to use enviroment variabls
 const pool = new Pool({
   user: USER,
   password: PASSWORD,
   host: HOST,
   port: DBPORT,
   database: DBNAME,
-  max: '30',
+  max: '10000',
+  idleTimeoutMillis: '1000',
+
 });
 
 module.exports = { pool };
